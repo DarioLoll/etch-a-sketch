eb8b82c6b0ee77a00ab09e4523c0fc5681c7df4c
DarioLoll <stromajer.dario@gmail.com>
2024-09-10 22:34:37 +0200

    Implement grid generation, add navbar

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Hello World!");
+generateGrid(64);
+
+function generateGrid(size) {
+    const container = document.querySelector(".grid");
+
+    let containerWidth = getComputedStyle(container).width;
+    containerWidth = parseInt(containerWidth.slice(0, -2));
+    let cellSize = containerWidth  / size;
+
+    for (let i = 0; i < size * size; i++) {
+        let cell = document.createElement("div");
+        cell.classList.toggle("cell");
+        cell.style.width = cellSize + "px";
+        cell.style.height = cellSize + "px";
+        container.appendChild(cell);
+    }
+}
